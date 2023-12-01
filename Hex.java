2f1dfab415838261650d7c427e75a401daf1c78e
Ariles Fellah <ariles.fellah937@gmail.com>
2023-12-01 22:07:00 +0100

    demande du choix du mode avec message d'erreur

--- Hex.java
+++ Hex.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 public class Hex{
-    Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String mode;
+        do{
+            System.out.print("Veuillez choisir un mode de jeu :\n\n(1) pour le mode Classique\n(2) pour le mode Aleatoire\n(3) pour le mode Special\n\n>> ");
+            mode = sc.nextLine();
 
+            if((!mode.equals("1")) && (!mode.equals("2")) && (!mode.equals("3")))
+                System.out.println("Erreur, la réponse n'est pas admise\n");
+    
+        }while(!mode.equals("1") && !mode.equals("2") && !mode.equals("3"));
+
+        sc.close();
     }
 }
