addddabba671f3b211a810935499594db54bf7da
Ariles Fellah <ariles.fellah937@gmail.com>
2023-12-01 22:20:03 +0100

    Creation de la methode

--- Hex.java
+++ Hex.java
@@ -4,4 +4,14 @@ public class Hex{
     public static void main(String[] args) {
 
     }
+    public static void Classique() {
+        int[][] plateau = new int[5][5];
+
+        for(int i = 0;i<plateau.length;i++){
+            for(int j = 0;j<plateau[i].length;j++){
+                System.out.print(plateau[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
