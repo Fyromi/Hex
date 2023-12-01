45a29b0d224ece3c5f8e28c98186cc5b0b042d4d
Ariles Fellah <ariles.fellah937@gmail.com>
2023-12-01 22:30:35 +0100

    Creation d'une Methode Choix

--- Hex.java
+++ Hex.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
 public class Hex{
     public static void main(String[] args){
+    }
+
+    public static int Mode() {
         Scanner sc = new Scanner(System.in);
-        String mode;
+        String cMode;
+        byte bMode;
+
         do{
             System.out.print("Veuillez choisir un mode de jeu :\n\n(1) pour le mode Classique\n(2) pour le mode Aleatoire\n(3) pour le mode Special\n\n>> ");
-            mode = sc.nextLine();
+            cMode = sc.nextLine();
 
-            if((!mode.equals("1")) && (!mode.equals("2")) && (!mode.equals("3")))
+            if((!cMode.equals("1")) && (!cMode.equals("2")) && (!cMode.equals("3")))
                 System.out.println("Erreur, la réponse n'est pas admise\n");
     
-        }while(!mode.equals("1") && !mode.equals("2") && !mode.equals("3"));
+        }while(!cMode.equals("1") && !cMode.equals("2") && !cMode.equals("3"));
 
+        if(cMode.equals("1"))
+            bMode =1;
+        else if(cMode.equals("2"))
+            bMode =2;
+        else
+            bMode =3;
+        
         sc.close();
+
+        return bMode;
+        
     }
 }
