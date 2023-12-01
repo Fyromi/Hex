ef055d0673edec241f276cb6f1be41bb8dfda06e
Ariles Fellah <ariles.fellah937@gmail.com>
2023-12-01 22:35:52 +0100

    Ajout appels des différentes Methodes dans main

--- Hex.java
+++ Hex.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 public class Hex{
     Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-
+        if(Mode()==1)
+            Classique();
+        else if(Mode()==2)
+            Aleatoir();
+        else
+            Special();
     }
 }
